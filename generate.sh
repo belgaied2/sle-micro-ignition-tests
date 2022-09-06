8edc82ca06b288010c0632a003120ef613eea7e3
Dominic Giebert <dominic.giebert@suse.com>
2022-09-06 14:38:00 +0200

    Use plain mount

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 IGNITION_RAW=ignition.img
+IGNITION_MOUNT="mnt"
+
 if [ ! -f "$IGNITION_RAW" ]; then
     echo "Creating $IGNITION_RAW"
     dd if=/dev/zero of=$IGNITION_RAW bs=1 count=0 seek=10M
@@ -8,16 +10,18 @@ if [ ! -f "$IGNITION_RAW" ]; then
     e2label $IGNITION_RAW ignition
 fi
 
-IGNITION_MOUNT="/run/media/$(whoami)/ignition"
-
-echo "Creating Ignition + Combustion Image"
-udisksctl loop-setup -f $IGNITION_RAW
-sleep 1
+echo "Mounting $IGNITION_RAW"
+[ -d $IGNITION_MOUNT ] mkdir $IGNITION_MOUNT
+sudo mount $IGNITION_RAW $IGNITION_MOUNT
 
 echo "Creating Ignition"
 butane -p ignition/config.fcc -o ignition/config.ign
 
+echo "Copying combustion + ignition"
 sudo rm -rf $IGNITION_MOUNT/combustion/*  $IGNITION_MOUNT/ignition/*
 sudo cp -r ./combustion $IGNITION_MOUNT
 sudo cp -r ./ignition  $IGNITION_MOUNT
-umount $IGNITION_MOUNT
+
+echo "Cleaning up"
+sudo umount $IGNITION_MOUNT
+sudo rm -r $IGNITION_MOUNT
